1ae54443d607c5e20a7a3e82a65c2e7e496b7736
lujin <lujinmohammed223@gmail.com>
2024-09-29 14:56:43 +0300

    Initial Commit

---
+++ preasentMe/preasentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  preasentMeApp.swift
+//  preasentMe
+//
+//  Created by lujin mohammed on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct preasentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
